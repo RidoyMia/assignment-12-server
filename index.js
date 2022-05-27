25290bb0bf11f882eb9eb456ec288e3a0f2d35f1
RidoyMia <Ridoymia153159hero@gmail.com>
2022-05-27 18:31:51 +0600

    user load done

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ async function run(){
         const result = await cursor.toArray();
         res.send(result)
       })
+      //
       app.get('/order/:email',async(req,res)=>{
         const email = req.params.email;
         const query = {email : email};
